a4b4d285c9fd063a92918557c43dfde016a0346a
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-01-05 21:06:47 -0300

    if or else

--- main.js
+++ main.js
@@ -7,8 +7,10 @@ function playNote(event){ // essa vai ser a função executada após a gente cli
   // keycode
   let keyCode // criar uma variavel vazia
 
-  const iskeyboard = event.type === "keydown" // esta expressão de 3 iguais é exatamente igual, está peguntando se o evento é exatamente igual a keydown
-  if(iskeyboard) {
+  const iskeyboard = event.type === "keydown" // esta expressão de 3 iguais é exatamente igual, está peguntando se o evento é exatamente igual a keydown ent ele me responde veradadeiro ou falso
+  if(iskeyboard) { // ou seja sempre que o event.type for keydown ele entra nessa estrutura if
+
+  } else { // mas se for falso ele entra no else
 
   }
 
